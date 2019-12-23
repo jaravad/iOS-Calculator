5eb3c30bc70adeaa54c3ddd5fd641e6c2002fb4b
jesu2607 <jaravad@uninorte.edu.co>
2019-12-22 23:52:28 -0500

    dash for negative numbers, ac, just numbers appear

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const buttonsContainer = document.querySelector(".buttons");
 buttonsContainer.addEventListener("click", function(event) {
   const disp = document.querySelector(".display");
   if (event.target.tagName === "BUTTON") {
-    if (disp.innerText === "0") {
-      disp.innerText = event.target.innerText;
-    } else {
-      disp.innerText += event.target.innerText;
+    if (
+      !isNaN(event.target.innerText) ||
+      (event.target.innerText === "." && !disp.innerText.includes("."))
+    ) {
+      if (disp.innerText === "0") {
+        disp.innerText = event.target.innerText;
+      } else {
+        disp.innerText += event.target.innerText;
+      }
+    } else if (event.target.innerText === "AC") {
+      disp.innerText = "0";
+    } else if (event.target.innerText === "+/-" && disp.innerText !== "0") {
+      if (!disp.innerText.includes("-")) {
+        disp.innerText = "-" + disp.innerText;
+      } else {
+        disp.innerText = disp.innerText.replace("-", "");
+      }
     }
   }
 });
