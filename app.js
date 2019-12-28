a19be34a313e3bbae533d8243749d4e3455c357a
jesu2607 <jaravad@uninorte.edu.co>
2019-12-27 20:50:11 -0500

    basic operations working now

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const buttonsContainer = document.querySelector(".buttons");
+const disp = document.querySelector(".display");
+let operation = "";
+function howManyDots(str) {
+  if (str.length % 3 === 0) {
+    return parseInt(str.length / 3 - 1);
+  } else {
+    return parseInt(str.length / 3);
+  }
+}
+
 buttonsContainer.addEventListener("click", function(event) {
-  const disp = document.querySelector(".display");
   if (event.target.tagName === "BUTTON") {
     if (
       !isNaN(event.target.innerText) ||
-      (event.target.innerText === "." && !disp.innerText.includes("."))
+      (event.target.innerText === "," && !disp.innerText.includes(","))
     ) {
       if (disp.innerText === "0") {
         disp.innerText = event.target.innerText;
@@ -13,12 +22,30 @@ buttonsContainer.addEventListener("click", function(event) {
       }
     } else if (event.target.innerText === "AC") {
       disp.innerText = "0";
+      operation = "";
     } else if (event.target.innerText === "+/-" && disp.innerText !== "0") {
       if (!disp.innerText.includes("-")) {
         disp.innerText = "-" + disp.innerText;
       } else {
         disp.innerText = disp.innerText.replace("-", "");
       }
+    } else if (
+      event.target.innerText === "+" ||
+      event.target.innerText === "-"
+    ) {
+      operation += disp.innerText + event.target.innerText;
+      disp.innerText = "0";
+    } else if (event.target.innerText === "x") {
+      operation += disp.innerText + "*";
+      disp.innerText = "0";
+    } else if (event.target.innerText === "÷") {
+      operation += disp.innerText + "/";
+      disp.innerText = "0";
+    } else if (event.target.innerText === "=") {
+      operation += disp.innerText;
+      disp.innerText = eval(operation);
+      operation = "";
+      console.log(howManyDots(disp.innerText));
     }
   }
 });
