1743b205a5690a0f4f68037830e9d509e29a7392
jaravad <jaravad@uninorte.edu.co>
2020-06-30 20:06:22 -0500

    button active transition, minor visual changes

--- app.js
+++ app.js
@@ -1,50 +1,42 @@
-const buttonsContainer = document.querySelector(".buttons");
-const opDisp = document.querySelector(".op-display");
-const disp = document.querySelector(".display");
-let operation = "";
+const buttonsContainer = document.querySelector('.buttons');
+const opDisp = document.querySelector('.op-display');
+const disp = document.querySelector('.display');
+let operation = '';
 let sw = false; // switch to know if an operation is active or not
 
-function howManyDots(str) {
-  if (str.length % 3 === 0) {
-    return parseInt(str.length / 3 - 1);
-  } else {
-    return parseInt(str.length / 3);
-  }
-}
-
-buttonsContainer.addEventListener("click", function (event) {
+buttonsContainer.addEventListener('click', function (event) {
   // checking if a button was clicked inside the container
-  if (event.target.tagName === "BUTTON") {
+  if (event.target.tagName === 'BUTTON') {
     if (
       !isNaN(event.target.innerText) ||
-      (event.target.innerText === "." && !disp.innerText.includes("."))
+      (event.target.innerText === '.' && !disp.innerText.includes('.'))
     ) {
-      if (disp.innerText === "0") {
+      if (disp.innerText === '0') {
         disp.innerText = event.target.innerText;
       } else {
         if (sw === true) {
-          disp.innerText = "";
+          disp.innerText = '';
           disp.innerText += event.target.innerText;
           sw = false;
         } else {
           disp.innerText += event.target.innerText;
         }
       }
-    } else if (event.target.innerText === "AC") {
-      disp.innerText = "0";
-      operation = "";
-      opDisp.innerText = "";
+    } else if (event.target.innerText === 'AC') {
+      disp.innerText = '0';
+      operation = '';
+      opDisp.innerText = '';
       sw = false;
-    } else if (event.target.innerText === "+/-" && disp.innerText !== "0") {
-      if (!disp.innerText.includes("-")) {
-        disp.innerText = "-" + disp.innerText;
+    } else if (event.target.innerText === '+/-' && disp.innerText !== '0') {
+      if (!disp.innerText.includes('-')) {
+        disp.innerText = '-' + disp.innerText;
       } else {
-        disp.innerText = disp.innerText.replace("-", "");
+        disp.innerText = disp.innerText.replace('-', '');
       }
     } else if (
       // add and substract
-      event.target.innerText === "+" ||
-      event.target.innerText === "-"
+      event.target.innerText === '+' ||
+      event.target.innerText === '-'
     ) {
       if (sw !== true) {
         operation += disp.innerText + event.target.innerText;
@@ -54,36 +46,33 @@ buttonsContainer.addEventListener("click", function (event) {
       } else {
         operation = operation.replace(/.$/, event.target.innerText);
       }
-      console.log(operation);
-    } else if (event.target.innerText === "x") {
+    } else if (event.target.innerText === 'x') {
       // multiplication
       if (sw !== true) {
-        operation += disp.innerText + "*";
+        operation += disp.innerText + '*';
         opDisp.innerText = operation;
         sw = true;
         disp.innerText = eval(operation.substring(0, operation.length - 1));
       } else {
-        operation = operation.replace(/.$/, "*");
+        operation = operation.replace(/.$/, '*');
       }
-      console.log(operation);
-    } else if (event.target.innerText === "÷") {
+    } else if (event.target.innerText === '÷') {
       // division
       if (sw !== true) {
-        operation += disp.innerText + "/";
+        operation += disp.innerText + '/';
         opDisp.innerText = operation;
         sw = true;
         disp.innerText = eval(operation.substring(0, operation.length - 1));
       } else {
-        operation = operation.replace(/.$/, "/");
+        operation = operation.replace(/.$/, '/');
       }
-    } else if (event.target.innerText === "=") {
+    } else if (event.target.innerText === '=') {
       // equal button
       operation += disp.innerText;
       disp.innerText = eval(operation);
       opDisp.innerText = operation;
-      operation = "";
-      console.log(howManyDots(disp.innerText));
-    } else if (event.target.innerText === "%") {
+      operation = '';
+    } else if (event.target.innerText === '%') {
       disp.innerText = disp.innerText / 100;
     }
   }
